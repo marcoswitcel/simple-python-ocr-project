f5d2d5772d7c636e60ca301454771e9af6f82ae8
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-09-01 22:30:32 -0300

    primeira versão baseada no tutorial

--- src/main.py
+++ src/main.py
@@ -3,3 +3,48 @@ import tensorflow_datasets as tfds
 
 print("Olá mundo dos projetos de Machine Learning em Python")
 
+# carrega dados do dataset 'mnist' e 'decompõe' nas várias para fácil acesso
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+# montando a pipeline de treinamento
+def normalize_img(image, label):
+    """Normalize imgaes: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+
+# montando a pipeline de avaliação
+ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+# criando e treinand o modelo
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10),
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
+)
+
+model.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test
+)
