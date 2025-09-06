cdf9fb0c4e97288d5351181e9ed66384bccc035b
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-09-05 23:44:29 -0300

    mais dados

--- src/view-dataset.py
+++ src/view-dataset.py
@@ -3,7 +3,10 @@ import tensorflow_datasets as tfds
 # carrega dados do dataset 'mnist' e 'decompõe' nas várias para fácil acesso
 ds_train, ds_info = tfds.load('mnist', split='train', with_info=True)
 
-tfds.visualization.show_examples(ds=ds_train, ds_info=ds_info)
+# printando especificação dos elementos
+print(ds_train.element_spec)
+
+tfds.show_examples(ds=ds_train, ds_info=ds_info, rows=4, cols=4)
 
 
 
