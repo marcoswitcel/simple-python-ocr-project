0a90768f04a3df2011f94289a9af05bb7d3107b9
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-09-07 00:33:29 -0300

    predição com valor aleatório

--- src/load-model.py
+++ src/load-model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Carrega um modelo para testar predições')
@@ -15,3 +16,18 @@ model = tf.keras.models.load_model(model_filename)
 model.summary()
 
 # todo João, fazer predições com o modelo
+
+# gerando dados por hora, mas o formato seria esse
+image = np.random.rand(28, 28, 1).astype('float32')
+
+# remodelando para incluir a dimensão extra do batch
+data = np.expand_dims(image, axis=0)
+
+# faz predição e retorna uma lista de probabilidades
+predictions = model.predict(data)
+
+print(f"Predições: {predictions}")
+
+# Achando o índice da maior probabilidade
+predicted_index = np.argmax(predictions)
+print(f"Classe prevista: {predicted_index}")
